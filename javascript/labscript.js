0dcda37f9f8ce864e93d806ab338aeec025d7d96
mitsn437 <mitsn437@newschool.edu>
2015-10-27 18:33:24 +0000

    step4

---
+++ javascript/labscript.js
@@ -0,0 +1,12 @@
+$("body").; html("Hello world...") //html is a function inside of jquery which is repeated every time it is ran
+
+
+function myFunction(){
+    console.log("hello world");
+}
+
+function addNums(num1, num2) {
+    //var sum=num1 - num2;
+    //return sum;
+    console.log(num1 = num2);
+}
