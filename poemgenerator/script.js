f497398a45a5e293fa71460636f1b1310eae18c6
mitsn437 <mitsn437@newschool.edu>
2015-12-11 22:28:18 +0000

    new

---
+++ poemgenerator/script.js
@@ -0,0 +1,8 @@
+var smallpoem = ["She is thin because she went up a hill", "She is thin because she can swim", "She is thin because she will not sit", "His head is small since he hit a wall", "His head is small since he will only play ball", "His head is small since he is above all", "He might have a small heart but his experience was off the chart", "He might have a small heart but it was set on art", "He might have a small heart but he knew a lot about Greek art"];
+
+var smallimage = ["hill.jpg", "slim.png", "cactus.png", "wall.jpg", "adriano.jpeg", "shaggy.jpg", "graph.png", "jeff.png", "greek.png"];
+
+
+var bigpoem = ["His blood filled the whole sea, that is how big his body would be", "He could not even swim in the sea, that is how big his body would be", "A ship could only carry him in the sea, that is how big his body would be", "He had such a big head maybe it was because he read", "He had such a big head he found every book easy to be read", "He had such a big head he could talk to the dead", "He has such a big heart but don’t even make me start", "He has such a big heart but he loves sugar more than art", "He has such a big heart but I don’t know what to write in this part"];
+
+var bigimage = ["whale.png", "fatcat.jpg", "bear.png", "dog.jpg", "argon.jpg", "rip.jpg", "annoyed.jpg", "willy.jpg", "avocado.png"];
